56bd409d458a3abd8bc141d77375c66cf8a14af4
Tyy996 <houle.tho@gmail.com>
2025-01-27 18:33:36 -0600

    Early stage of implementing Requirements for Trait, Ability, and Material elements

---
+++ DataAccess/Models/RequirementModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DataAccess.Models
+{
+    public class RequirementModel
+    {
+        public int Id { get; set; }
+        public string ElementType { get; set; }
+        public int ElementID { get; set; }
+    }
+}
